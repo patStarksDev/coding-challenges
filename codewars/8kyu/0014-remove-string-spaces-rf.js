e33c7fb7b15dff1727a932bb0c0e63f2e714c172
patStarksDev <patstarks.dev@gmail.com>
2022-04-14 20:05:28 -0400

    2022-04-14 refactored yesterday's challenge; used regex

---
+++ codewars/8kyu/0014-remove-string-spaces-rf.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+
+// Remove String Spaces
+
+// Description:
+
+// Simple, remove the spaces from the string, then return the resultant string.
+
+function noSpace(x) {
+    return x.replace(/ /g, "");
+}
+
+// 2022-04-14
