3dedf9a09cf4fa164c8ab19ce83001883386e39b
patStarksDev <patstarks.dev@gmail.com>
2022-04-20 21:24:41 -0400

    2022-04-20 fixed broken code; added return before expression within function body

---
+++ codewars/8kyu/0020-multiply.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/50654ddff44f800200000004
+
+// Multiply
+
+// Description:
+
+// This code does not execute properly. Try to figure out why.
+
+// function multiply(a, b) {
+//     a * b;
+// }
+
+function multiply(a, b) {
+    return a * b;
+}
+
+// 2022-04-20
