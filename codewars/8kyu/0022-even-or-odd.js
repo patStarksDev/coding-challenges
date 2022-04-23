c56fac274ee7c4b077e213d377fc964300a26a12
patStarksDev <patstarks.dev@gmail.com>
2022-04-22 22:05:54 -0400

    2022-04-22 return whether given number is even or odd; used if-else conditional, remainder operator

---
+++ codewars/8kyu/0022-even-or-odd.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+// Even or Odd
+
+// Description:
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function even_or_odd(number) {
+    if (number % 2 === 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
+
+// 2022-04-22
