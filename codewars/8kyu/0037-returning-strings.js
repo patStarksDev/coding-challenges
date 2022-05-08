9ceb878173e87e042b36f53263a404e346e0510a
patStarksDev <patstarks.dev@gmail.com>
2022-05-07 23:29:37 -0400

    2022-05-07 concatenate argument within string; used template literal

---
+++ codewars/8kyu/0037-returning-strings.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/55a70521798b14d4750000a4
+
+// Returning Strings
+
+// Description:
+
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+function greet(name) {
+    return `Hello, ${name} how are you doing today?`;
+}
+
+// 2022-05-07
