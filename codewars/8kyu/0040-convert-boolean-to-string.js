7aabf6ad88bed8711f7ae802083e223defd9a9bd
patStarksDev <patstarks.dev@gmail.com>
2022-05-10 23:48:00 -0400

    2022-05-10 return string for given boolean; used ternary operator

---
+++ codewars/8kyu/0040-convert-boolean-to-string.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/53369039d7ab3ac506000467
+
+// Convert boolean values to strings 'Yes' or 'No'.
+
+// Description:
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord(bool) {
+    return bool ? "Yes" : "No";
+}
+
+// 2022-05-10
