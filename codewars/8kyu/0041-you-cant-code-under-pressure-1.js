a2e784e4b3b37872ca97af5f960a90af0a929f5b
patStarksDev <patstarks.dev@gmail.com>
2022-05-11 23:50:34 -0400

    2022-05-11 return product of argument and two; used math

---
+++ codewars/8kyu/0041-you-cant-code-under-pressure-1.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/53ee5429ba190077850011d4
+
+// You Can't Code Under Pressure #1
+
+// Description:
+
+// Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+    return 2 * i;
+}
+
+// 2022-05-11
