90a46e6d4f56e163b6200f62a4b353b6f6e68f37
patStarksDev <patstarks.dev@gmail.com>
2022-05-16 00:54:49 -0400

    2022-05-16 return uppercased string; used str.toUpperCase() method

---
+++ codewars/8kyu/0046-makeuppercase.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+
+// MakeUpperCase
+
+// Description:
+
+// Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+    return str.toUpperCase();
+}
+
+// 2022-05-16
