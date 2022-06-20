bd0a1489f3d7419da99aa51780235fdb2e3ecc78
patStarksDev <patstarks.dev@gmail.com>
2022-06-20 19:58:56 -0400

    2022-06-20 return product of given arguments; used multiplication

---
+++ codewars/8kyu/0081-multiplying-two-numbers.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/523b66342d0c301ae400003b
+
+// Function 3 - multiplying two numbers
+
+// Description:
+
+// Implement a function which multiplies two numbers.
+
+function multiply(x, y) {
+    return x * y;
+}
+
+// 2022-06-20
