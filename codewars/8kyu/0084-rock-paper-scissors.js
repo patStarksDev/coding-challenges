cbd0816fa0faefe0234508fb2793a2e04f59cb57
patStarksDev <patstarks.dev@gmail.com>
2022-06-23 20:17:51 -0400

    2022-06-23 return string based on victory conditions; used nested ternary, logical and, equals operators

---
+++ codewars/8kyu/0084-rock-paper-scissors.js
@@ -0,0 +1,27 @@
+// https://www.codewars.com/kata/5672a98bdbdd995fad00000f
+
+// Rock Paper Scissors!
+
+// Description:
+
+// Rock Paper Scissors
+
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples(Input1, Input2 --> Output):
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+const rps = (p1, p2) => {
+    return p1 === p2
+        ? "Draw!"
+        : (p1 === "rock" && p2 === "scissors") ||
+          (p1 === "paper" && p2 === "rock") ||
+          (p1 === "scissors" && p2 === "paper")
+        ? "Player 1 won!"
+        : "Player 2 won!";
+};
+
+// 2022-06-23
