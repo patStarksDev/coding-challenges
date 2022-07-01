6b46e326678a223ce783d6d9326611e711bee4ce
patStarksDev <patstarks.dev@gmail.com>
2022-06-30 22:59:02 -0400

    2022-06-30 refactored yesterday's challenge; used str.split() with targeted character as separator

---
+++ codewars/8kyu/0091-remove-exclamation-marks-rf copy.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/57a0885cbb9944e24c00008e
+
+// Remove exclamation marks
+
+// Description:
+
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+    return s.split("!").join("");
+}
+
+// 2022-06-30
