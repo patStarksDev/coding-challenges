354aaf28a3c9e7619672b589968fecf30294850d
patStarksDev <patstarks.dev@gmail.com>
2022-07-05 23:25:41 -0400

    2022-07-05 evaluate whether three numbers can be lengths of a triangle, return boolean; used arr.sort(), .every(), math

---
+++ codewars/7kyu/0096-is-this-a-triangle.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/56606694ec01347ce800001b
+
+// Is this a triangle?
+
+// Description:
+
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+
+function isTriangle(a, b, c) {
+    const sides = [a, b, c].sort((a, b) => a - b);
+    return sides.every((e) => e > 0) && sides[0] + sides[1] > sides[2]
+        ? true
+        : false;
+}
+
+// 2022-07-05
