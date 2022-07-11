ee3785ad10ecda6e1d91c68f02e9826102c260e1
patStarksDev <patstarks.dev@gmail.com>
2022-07-11 13:46:53 -0400

    2022-07-11 given string, replace specific characters, return string; used arr.map(), nested ternary operators

---
+++ codewars/7kyu/0110-complementary-dna.js
@@ -0,0 +1,27 @@
+// https://www.codewars.com/kata/554e4a2f232cdd87d9000038
+
+// Complementary DNA
+
+// Description:
+
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+// If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+// Example: (input --> output)
+
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+
+function DNAStrand(dna) {
+    return dna
+        .split("")
+        .map((e) => {
+            return e === "A" ? "T" : e === "C" ? "G" : e === "G" ? "C" : "A";
+        })
+        .join("");
+}
+
+// 2022-07-11
