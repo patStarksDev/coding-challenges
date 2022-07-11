ed6a279b045210184b8e78c8669c26ec3e123684
patStarksDev <patstarks.dev@gmail.com>
2022-07-11 16:59:02 -0400

    2022-05-24 evaluate expression, return number; used math

---
+++ codewars/8kyu/0054-volume-of-a-cuboid.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/58261acb22be6e2ed800003a
+
+// Volume of a Cuboid
+
+// Description:
+
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+class Cuboid {
+    static getVolume(length, width, height) {
+        return length * width * height;
+    }
+}
+
+// 2022-05-24
